8aac17385960cac21fd00681d61d1c196d369f90
Hugo Correa <hugosalescorrea@gmail.com>
2017-10-02 23:54:11 -0300

    matrix multiplication for calculating fibonacci numbers

--- fib/fib.py
+++ fib/fib.py
@@ -20,3 +20,32 @@ def linearFibonacci(n):
 		return memory[n]
 
 	return rec(n)
+
+
+def mul2x2(m1, m2):
+	return ((m1[0][0]*m2[0][0] + m1[0][1]*m2[1][0], 
+		m1[0][0]*m2[0][1] + m1[0][1]*m2[1][1]),
+		(m1[1][0]*m2[0][0]+m1[1][1]*m2[1][0],
+			m1[1][0]*m2[0][1]+m1[1][1]*m2[1][1]))
+
+def matrixFibonacci(n):
+
+
+	m = ((1, 1), (1, 0))
+
+	def nthPower(n):
+
+		if n == 1:
+			return m
+
+		if n % 2 == 0:
+			p = nthPower(n/2)
+			return mul2x2(p, p)
+		else:
+			p = nthPower((n-1)/2)
+			return mul2x2(m, mul2x2(p, p))
+
+	if n <= 1:
+		return n
+	else:
+		return nthPower(n)[1][0]
